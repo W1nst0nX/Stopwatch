cfe685fe079854885689ed76a1e2c15f38142177
Winston <winstonx2004@gmail.com>
2020-09-29 10:48:57 -0700

    Added loggs and finished stopwatch

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,63 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+    var timeWhenStopped:Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        chronometer_main_timer.stop()
+
+        button_main_start.setOnClickListener {
+            chronometer_main_timer.base = SystemClock.elapsedRealtime() + timeWhenStopped
+            chronometer_main_timer.start()
+        }
+
+        button_main_stop.setOnClickListener {
+            timeWhenStopped = chronometer_main_timer.base - SystemClock.elapsedRealtime()
+            chronometer_main_timer.stop()
+        }
+
+        button_main_reset.setOnClickListener {
+            chronometer_main_timer.base = SystemClock.elapsedRealtime()
+            timeWhenStopped = 0
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart has been called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume has been called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause has been called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop has been called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart has been called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy has been called")
     }
 }
